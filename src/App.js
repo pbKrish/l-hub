95b159301a13e981fc876916f9a8c2648e3779cd
pbKrish <pbKrish07@gmail.com>
2017-10-22 00:58:08 -0700

    Mobile App Menu

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import ReactDOM from 'react-dom';
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -11,44 +13,85 @@ import {
 import Home from "./Home.js";
 import About from "./About.js";
 import AppBar from 'material-ui/AppBar';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MainMenu from './MainMenu.js';
+const styles = {
+  customWidth: {
+    width: 175,
+  },
+};
 
 
-class App extends Component {
+var createReactClass = require('create-react-class');
+
+const App = createReactClass({
+getInitialState(){
+        return {
+            open: false,
+        }
+    },
+
+
+  
+renderMenu(){
+if(!this.state.open){
+     ReactDOM.render(<MainMenu />, document.getElementById('menu'));
+   this.setState({open: true});}
+else{
+     ReactDOM.render(<div></div>, document.getElementById('menu'));
+   this.setState({open: false});}
+
+},
+
   render() {
     return (
       <div className="App">
+        <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    
+      <button onClick={this.renderMenu} type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/l-hub">Learn A.I</a>
+ 
+    </div>
+     <div id="menu"></div>
+
+    <div class="collapse navbar-collapse" id="main-menu">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/l-hub/home">Regression</a></li>
+       <li><a href="/l-hub/about">Classification</a></li>
+       <li><a href="/l-hub/about">Clustering</a></li>
+       <li><a href="/l-hub/about">Neural Networks</a></li>
+       <li><a href="/l-hub/about">Reinforcemnet Learning</a></li>
+       <li><a href="/l-hub/about">Bonus</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
       <MuiThemeProvider>
 
       <Router>
+<div>
     <div>
-       <nav>
-    <div class="nav-wrapper">
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <a href="/l-hub/" class="brand-logo left">Deep Learning</a>
-     </ul>
-         <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><Link to="/l-hub/home">Regression</Link></li>
-        <li><Link to="/l-hub/about">Classification</Link></li>
-        <li><Link to="/l-hub/about">Clustering</Link></li>
-        <li><Link to="/l-hub/about">Neural Networks</Link></li>
-        <li><Link to="/l-hub/about">Reinforcement Learning</Link></li>
-        <li><Link to="/l-hub/about">Bonus</Link></li>
-        </ul>
-    </div>
-  </nav>
 
       <Route exact path="/l-hub/home" component={Home}/>
-      <Route path="/l-hub/about" component={About}/>
-               
- 
-    </div>
-  </Router>
-
+      <Route path="/l-hub/about" component={About}/>      
+</div>
+</div>
+</Router>
 </MuiThemeProvider>
       </div>
 
     );
   }
-}
+})
 
 export default App;
---
+++ src/MainMenu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class MainMenu extends React.Component{
+render(){
+	return (
+<div >
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/l-hub/home">Regression</a></li>
+        <li><a href="/l-hub/about">Classification</a></li>
+        <li><a href="/l-hub/home">Regression</a></li>
+        <li><a href="/l-hub/about">Classification</a></li>
+      <li><a href="/l-hub/home">Regression</a></li>
+        <li><a href="/l-hub/about">Classification</a></li>
+      
+      </ul>
+
+</div>
+);
+}
+}
+
+export default MainMenu ;
