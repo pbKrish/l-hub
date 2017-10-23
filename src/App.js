275cf25f1645095d06a05870c13d8b2c6e856f03
pbKrish <pbKrish07@gmail.com>
2017-10-22 23:31:11 -0700

    dropdown-perfection

--- src/App.js
+++ src/App.js
@@ -49,15 +49,18 @@ else{
   render() {
     return (
       <div className="App">
+      <Router>
+      <MuiThemeProvider>
+
         <nav className="navbar navbar-default">
   <div className="container-fluid">
     <div className="navbar-header">
       
-      <div className="navbar-toggle " >
+      <div className="navbar-toggle mobnav" >
 
     <DropdownButton title="Dropdown" id="bg-nested-dropdown">
-      <MenuItem eventKey="1" href="/l-hub/home">Regression</MenuItem>
-      <MenuItem eventKey="2" href="/l-hub/about">Classification</MenuItem>
+      <MenuItem eventKey="1"><Link to="/l-hub/home"><a style={{color: "black"}}>Regression</a></Link></MenuItem>
+      <MenuItem eventKey="2"><Link to="/l-hub/about"><a style={{color: "black"}}>Classification</a></Link></MenuItem>
     </DropdownButton>
 </div>
       <a className="navbar-brand" href="/l-hub">Learn A.I</a>
@@ -67,12 +70,12 @@ else{
 
     <div className="collapse navbar-collapse" id="main-menu">
       <ul className="nav navbar-nav navbar-right">
-        <li><a href="/l-hub/home">Regression</a></li>
-       <li><a href="/l-hub/about">Classification</a></li>
-       <li><a href="/l-hub/about">Clustering</a></li>
-       <li><a href="/l-hub/about">Neural Networks</a></li>
-       <li><a href="/l-hub/about">Reinforcemnet Learning</a></li>
-       <li><a href="/l-hub/about">Bonus</a></li>
+        <li><Link to="/l-hub/home">Regression</Link></li>
+       <li><Link to="/l-hub/about">Classification</Link></li>
+       <li><Link to="/l-hub/about">Clustering</Link></li>
+       <li><Link to="/l-hub/about">Neural Networks</Link></li>
+       <li><Link to="/l-hub/about">Reinforcemnet Learning</Link></li>
+       <li><Link to="/l-hub/about">Bonus</Link></li>
       </ul>
     </div>
   </div>
@@ -80,18 +83,12 @@ else{
 
 
 
-      <MuiThemeProvider>
-
-      <Router>
-<div>
-    <div>
 
       <Route exact path="/l-hub/home" component={Home}/>
       <Route path="/l-hub/about" component={About}/>      
-</div>
-</div>
-</Router>
 </MuiThemeProvider>
+
+</Router>
 
 
 
